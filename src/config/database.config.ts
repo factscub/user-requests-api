68463087b3837607b8d62a47f778d46ba441c789
bethasiva <bethasiva7780@gmail.com>
2024-06-10 18:11:58 +0530

    database configuration keys added

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -13,12 +13,16 @@ export const databaseConfig = async (
   configService: ConfigService,
 ): Promise<TypeOrmModuleOptions> => {
   return {
-    type: configService.get('DATABASE_TYPE'), // Database type (e.g., postgres, mysql)
+    type: 'postgres',
     host: configService.get<string>('DATABASE_HOST'), // Database host address
     port: configService.get<number>('DATABASE_PORT'), // Database port number
     username: configService.get<string>('DATABASE_USERNAME'), // Database username
     password: configService.get<string>('DATABASE_PASSWORD'), // Database password
     entities: [Application, User], // List of entities (database tables) to be synchronized
     synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'), // Whether to auto-create database tables based on entity definitions
+    database: configService.get<string>('DATABASE_NAME'),
+    ssl: {
+      rejectUnauthorized: false,
+    },
   };
 };
